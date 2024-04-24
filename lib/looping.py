d8befda921fb8c78a640c0f3d5a2d77d0eaa158f
stella <sftamesis13@gmail.com>
2024-04-24 13:04:49 -0700

    complete pass all 3

--- lib/looping.py
+++ lib/looping.py
@@ -14,6 +14,14 @@ def square_integers(int_list):
     squared_number = [ i ** 2 for i in int_list]
     return squared_number
 
+# Write a function fizzbuzz() that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
