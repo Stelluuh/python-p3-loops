488402532384ed04d0b116ebaeee7c590ff6f85a
stella <sftamesis13@gmail.com>
2024-04-24 12:54:43 -0700

    pass 1

--- lib/looping.py
+++ lib/looping.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+# Write a function happy_new_year() using a while loop that outputs numbers starting at 10 and counting down to 1. After reaching 1, print out "Happy New Year!"
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
